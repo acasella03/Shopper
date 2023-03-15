1b436a43382de028e25111fdbd2c66d6bc502b09
Angi Marliu Casella <acasella@danielcastelao.org>
2023-03-15 18:53:35 +0100

    Creación de Clase FactoryContenedor y del menú en la main

--- src/com/shopper/supermercado.java
+++ src/com/shopper/supermercado.java
@@ -3,51 +3,47 @@ package com.shopper;
 import com.angi.datos.PedirDatos;
 import com.shopper.contenedores.Bolsa;
 import com.shopper.contenedores.Caja;
+import com.shopper.contenedores.FactoryContenedor;
 import com.shopper.productos.Congelado;
 import com.shopper.productos.Fresco;
 import com.shopper.productos.Higiene;
 
 public class supermercado {
     public static void main(String[] args) {
+        IPedido miPedido = null;
+        IContenedor bolsa = null;
+        IContenedor caja = null;
         int opcion;
-        do{
-            opcion = PedirDatos.pedirEntero("***MENU*** \n\n 1---> Crear Pedido \n 2---> Crear contenedor  \n 3---> Añadir contenedor al pedido \n\n Teclea una opcion de las anteriores");
+        do {
+            opcion = PedirDatos.pedirEntero("***MENU*** \n\n 1---> Crear Pedido \n 2---> Crear contenedor \n3--->Crear productos \n4--->Mostrar contenedor \n5--->Mostrar pedido \n6--->Salir \n\n Teclea una opcion de las anteriores");
             switch (opcion) {
                 case 1:
-                    IPedido miPedido = new Pedido(PedirDatos.pedirDatos("Escribe la referencia del pedido: "));
+                    miPedido = new Pedido(PedirDatos.pedirDatos("Escribe la referencia del pedido: "));
                     break;
 
                 case 2:
-                    IContenedor bolsa1 = new Bolsa("B111", 40, 25,900);
-                    IContenedor caja1 = new Caja("C222", 30, 50, 75);
+                    bolsa = FactoryContenedor.getContenedor(FactoryContenedor.BOLSA);
+                    caja = FactoryContenedor.getContenedor(FactoryContenedor.CAJA);
+                    miPedido.addContenedor(bolsa);
+                    miPedido.addContenedor(caja);
                     break;
-                case 3:
-                    miPedido.addContenedor(bolsa1);
-                    miPedido.addContenedor(caja1);
 
+                case 3:
+                    IProducto manzanas = new Fresco("MNZ", 1000, 1500);
+                    IProducto helado = new Congelado("HLD", 800, 1000);
+                    IProducto papelWC = new Higiene("PWC", 500, 2500);
+                    IProducto peras = new Fresco("PER", 800, 1200);
+                case 4:
+                    /*IContenedor contManzanas = miPedido.addProducto(manzanas);
+                    IContenedor contHelado = miPedido.addProducto(helado);
+                    IContenedor contPapel = miPedido.addProducto(papelWC);
+                    IContenedor contPeras = miPedido.addProducto(peras);*/
+                case 5:
+                    //System.out.println("Mi pedido con productos: " + miPedido);
                 default:
                     break;
             }
-        }while (opcion<=);
-
-
-
-
-
-        System.out.println("Bolsa: "+bolsa1);
-        System.out.println("Caja: "+caja1);
-        System.out.println("Mi pedido con contenedores: "+miPedido);
-
-        IProducto manzanas = new Fresco("MNZ", 1000, 1500);
-        IProducto helado = new Congelado("HLD", 800, 1000);
-        IProducto papelWC = new Higiene("PWC", 500, 2500);
-        IProducto peras = new Fresco("PER", 800, 1200);
-
-        IContenedor contManzanas = miPedido.addProducto(manzanas);
-        IContenedor contHelado = miPedido.addProducto(helado);
-        IContenedor contPapel = miPedido.addProducto(papelWC);
-        IContenedor contPeras = miPedido.addProducto(peras);
-        System.out.println("Mi pedido con productos: "+miPedido);
+        } while (opcion <= 5);
 
     }
 }
